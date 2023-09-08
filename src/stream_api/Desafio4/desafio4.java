00d9db1f1777c59cbec126ca144a5ca48d864679
henriquegouveia1 <henriquegs002@gmail.com>
2023-09-08 16:54:14 -0300

    desafio 4 executado

--- src/stream_api/Desafio4/desafio4.java
+++ src/stream_api/Desafio4/desafio4.java
@@ -1,7 +1,18 @@
 package stream_api.Desafio4;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class desafio4 {
     public static void main(String[] args) {
-        
+                List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, -4);
+
+               boolean resultado = numeros.stream().allMatch(t -> t>0);
+
+                if (resultado){
+                    System.out.println("Todos os números são positivos");
+                }else{
+                    System.out.println("Existem valores negativos na lista!");
+                }
     }
 }
