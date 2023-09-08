2d75bfa0d4d75ef7c22d9be01ea867ca0d85c6db
henriquegouveia1 <henriquegs002@gmail.com>
2023-09-08 16:36:56 -0300

    updating o 3

---
+++ src/stream_api/Desafio4/desafio4.java
@@ -0,0 +1,7 @@
+package stream_api.Desafio4;
+
+public class desafio4 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/stream_api/desafio3/desafio3.java
@@ -0,0 +1,20 @@
+package stream_api.desafio3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class desafio3 {
+    public static void main(String[] args) {
+        
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> numeros2 = new ArrayList<>(numeros);
+
+        
+        numeros2.removeIf(n -> n%2 != 0);
+        
+        System.out.println(numeros2);
+    }
+}
